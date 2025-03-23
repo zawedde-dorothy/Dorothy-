210dcd046e8a5cc85d1c1ff385be0c7ec36190df
zawedde-dorothy <zaweddedorothy7@gmail.com>
2025-03-23 06:15:05 -0700

    Add files via upload

---
+++ Zawedde.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+    int withdraw;
+    int amount = 50000;
+    scanf("&d", &withdraw);
+    amount = amount-withdraw;
+    prrintf("The amount is:%dshs \n");
+    return 0;
+}
